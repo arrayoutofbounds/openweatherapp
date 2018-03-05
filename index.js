794c24710064da45860e68f55672a44c9b978e15
Anmol Desai <anmoldesai4@gmail.com>
2018-03-05 17:55:10 +1300

    real time weather in a city

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
-app.set('view engine', 'ejs')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
+const apiKey = "cb577e6b8d77df016b4072dc0591acc3";
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
 
 app.get('/', function (req, res) {
   //res.send('Hello World!')
@@ -10,3 +17,21 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+app.post('/', function (req, res) {
+    let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees celcius in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+});
